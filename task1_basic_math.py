219febfeb38030f3cfeaf768ca2790883148fbf9
variakrupal <variakrupal219@gmail.com>
2025-09-07 21:55:16 +0530

    Create task1_basic_math.py

---
+++ task1_basic_math.py
@@ -0,0 +1,20 @@
+
+# Task 1: Perform Basic Mathematical Operations
+
+# Taking two numbers as input from the user
+a=input("Enter a first number:")
+b=input("Enter a second number:")
+a=int(a)
+b=int(b)
+
+add=a + b
+print("Addition: ",add)
+
+sub=a - b
+print("Subtraction: ",sub)
+
+mul=a * b
+print("Multiplication: ",mul)
+
+div=a / b
+print("Division: ",div)
